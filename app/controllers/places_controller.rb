ab6d5294c39cf8086f45238ac75c0663a463baf2
Kasey Crumpton <kasey.crumpton@kellogg.northwestern.edu>
2025-06-04 07:49:48 +0000

    De-Bug Places via migrate file

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,7 +2,7 @@ class PlacesController < ApplicationController
   before_action :authenticate_user
 
   def index
-    @places = Place.all
+    @places = Place.where({ "user_id" => @current_user["id"] })
   end
 
   def show
@@ -16,6 +16,7 @@ class PlacesController < ApplicationController
   def create
     @place = Place.new
     @place["name"] = params["name"]
+    @place["user_id"] = @current_user["id"]
     @place.save
     redirect_to "/places"
   end
